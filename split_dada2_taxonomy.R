60aa575b0ba5633f936b2e6916f3e84135d012f2
Mathieu Ramon <48122753+MathKarst@users.noreply.github.com>
2024-06-19 13:55:30 +0200

    Update split_dada2_taxonomy.R

--- split_dada2_taxonomy.R
+++ split_dada2_taxonomy.R
@@ -1,70 +1,71 @@
-# package 
+# Load the necessary libraries
 library(tidyverse)
 
-# Fonction to convert the DADA2 taxonomic fasta file to two mothur files : the taxa and the fasta file 
+# Function to read the DADA2 FASTA file and split it into two files
 split_dada2_taxonomy <- function(dada2_file, fasta_output, taxa_output) {
-  # Lire le fichier FASTA
+  # Read the FASTA file
   fasta_lines <- readLines(dada2_file, warn = FALSE)
   
-  # Checking and correction of the number of lines
+  # Check and add a new line at the end if necessary
   if (length(fasta_lines) %% 2 != 0) {
     fasta_lines <- c(fasta_lines, "")
   }
   
-  # List initialization 
+  # Initialize lists to store information
   ids <- c()
   sequences <- c()
   taxa <- c()
   
-  # screening of the FASTA file
+  # Loop through the FASTA file lines
   for (i in seq(1, length(fasta_lines), by = 2)) {
-    # Extraire l'en-tête et la séquence
+    # Extract the header and sequence
     header <- fasta_lines[i]
     sequence <- fasta_lines[i + 1]
     
-    # extraction of the id and taxonomy from the header
+    # Extract the taxonomy and ID from the header
     taxon <- gsub("^>", "", header)
     taxon_parts <- strsplit(taxon, ";")[[1]]
     id <- taxon_parts[length(taxon_parts)]
     taxonomy <- paste(taxon_parts[-length(taxon_parts)], collapse = ";")
+    # Add the semicolon at the end of the taxonomy
+    taxonomy <- paste0(taxonomy, ";")
     
-    # add the id and taxonomy to the lists
+    # Add the information to the lists
     ids <- c(ids, id)
     sequences <- c(sequences, sequence)
     taxa <- c(taxa, taxonomy)
   }
   
-  # dataframe for sequences
+  # Create a DataFrame for the sequences
   fasta_df <- data.frame(
     id = ids,
     sequence = sequences,
     stringsAsFactors = FALSE
   )
   
-  # dataframe for taxonomy
+  # Create a DataFrame for the taxonomy
   taxa_df <- data.frame(
     id = ids,
     taxonomy = taxa,
     stringsAsFactors = FALSE
   )
   
-  # FASTA file writting
+  # Write the sequences in FASTA format
   writeLines(
     paste0(">", fasta_df$id, "\n", fasta_df$sequence),
     con = fasta_output
   )
   
-  # TSV taxonomy writting
+  # Write the taxonomy in TSV format
   write.table(taxa_df, taxa_output, sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 }
 
+# Path to your DADA2 taxonomy file
+dada2_file <- "path/to/your/dada2_file.fasta"
 
-# Path to DADA2 taxonomy
-dada2_file <- "refDADA2.fasta"
+# Paths to the output files
+fasta_output <- "path/to/output/mothur_fasta.fasta"
+taxa_output <- "path/to/output/mothur_taxa.tax"
 
-# Path to output files
-fasta_output <- "ref_mothur.fasta"
-taxa_output <- "ref_mothur.tax"
-
-# function calling 
+# Call the function to split the file
 split_dada2_taxonomy(dada2_file, fasta_output, taxa_output)
